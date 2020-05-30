5a1c520d53187b250d85eda8a3d9878952c897dd
Alex Lewin <asl0028@auburn.edu>
2020-05-30 15:17:29 -0400

    Added routing

--- guzzler/src/App.js
+++ guzzler/src/App.js
@@ -5,19 +5,41 @@ import Header from './Components/navbar';
 import './App.css';
 import LandingScreen from './Components/LandingScreen';
 import { BrowserRouter as Router, Route, Link  } from "react-router-dom";
+import Diagnostics from './Components/Q1';
+import Simulator from './Components/CupSimulator'
 
 function App() {
-  const Landing = () => {return <LandingScreen, name= />};
-  const [screen, setscreen] = useState(Landing);
-
-  function handleClick() {
-      
-  } 
+//  const Landing = () => {return <LandingScreen, name= />};
+//  const [screen, setscreen] = useState(Landing);
+//
+//  function handleClick() {
+//      
+//  } 
 
   return (
     <div>
+      <nav>
+        <Link to="/">
+          <button>
+            Home
+          </button>
+        </Link>
+        <Link to="/diagnostics">
+          <button>
+            Diagnostics 
+          </button>
+        </Link>
+        <Link to="/CupSimulator">
+          <button>
+            CupSimulator
+          </button>
+        </Link>
+      </nav>
+
       <div>
-        {screen}
+        <Route exact path="/" component={LandingScreen} />
+        <Route exact path="/diagnostics" component={Diagnostics} />
+        <Route exact path="/simulator" component={Simulator} />
       </div>
     </div>
   );
