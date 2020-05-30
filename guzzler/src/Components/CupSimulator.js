8116401902485e6eb6fd712385003f2924282765
Alex Lewin <asl0028@auburn.edu>
2020-05-30 17:44:19 -0400

    Small edits

--- guzzler/src/Components/CupSimulator.js
+++ guzzler/src/Components/CupSimulator.js
@@ -3,8 +3,9 @@ import React from 'react';
 function CupSimulator() {
   return (
     <div>
-
       <h2>Try to fill the glass to the safe amount to drink!</h2>
+      
+      <div className="cup" />
 
     </div>
   )
